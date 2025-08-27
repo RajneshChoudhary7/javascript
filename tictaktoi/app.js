389a2b9de42f15db64ead4babf1ec7727063a6ed
Rajneshking <ahirwarrajnesh493@gmail.com>
2025-08-27 14:08:29 +0530

    this was  how i thing update with ai

--- tictaktoi/app.js
+++ tictaktoi/app.js
@@ -1,32 +1,32 @@
-let boxes = document.querySelectorAll(".box")
-let resetBtn = document.querySelector(".reset")
-let newGameBtn = document.querySelector(".new-game")
-let msgContainer = document.querySelector(".msg-container")
-let msg = document.querySelector("#msg")
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset");
+let newGameBtn = document.querySelector(".new-game");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-let turnO = true
+let turnO = true; // player O starts
 
+// ✅ Correct win patterns
 let winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,5,5],
-    [6,7,8],
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
 ];
 
-boxes.forEach((box) =>
-{
-    box.addEventListener("click" , () => {
-
-        if(turnO){
-            box.innerText = "O"
-            turnO = false
-        }
-        else {
-            box.innerText = "X"
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O";
+            box.style.color = "blue"; // O in blue
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            box.style.color = "red"; // X in red
             turnO = true;
         }
         box.disabled = true;
@@ -35,51 +35,44 @@ boxes.forEach((box) =>
     });
 });
 
-
-
-const showWinner = (winner)  => {
-    msg.innerText = `Congratulation , Winner is ${winner}`;
-    msgContainer.classList.remove("hide")
+const showWinner = (winner) => {
+    msg.innerText = `🎉 Congratulations, Winner is ${winner}!`;
+    msgContainer.classList.remove("hide");
     disableBoxes();
-}
+};
 
 const disableBoxes = () => {
-    for(let box of boxes){
+    for (let box of boxes) {
         box.disabled = true;
     }
-}
+};
 
 const enableBoxes = () => {
-    for(let box of boxes){
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
-}
+};
 
-const checkWinner  = () => {
-    for(pattern of winPatterns) {
-
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val != "" && pos2Val != "" && pos3Val != "" ){
-            if(pos1Val ===  pos2Val && pos2Val === pos3Val){
-                showWinner(pos1Val)
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
             }
         }
     }
-
-
-
-}
-
+};
 
 const resetGame = () => {
-    turnO = true
+    turnO = true;
     enableBoxes();
-    msgContainer.classList.add("hide")
-}
+    msgContainer.classList.add("hide");
+};
 
-newGameBtn.addEventListener("click" , resetGame)
-resetBtn.addEventListener("click" , resetGame)
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
