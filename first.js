e616bb1a703336d3ef4c7bf79a76c0af0fd7d8ed
Rajneshking <ahirwarrajnesh493@gmail.com>
2025-09-02 12:16:11 +0530

    no

--- first.js
+++ first.js
@@ -2,6 +2,8 @@
 // document.write("hello this i js file ")
 //     console.log('1')
 
+// const { use } = require("react");
+
 //     let arr =  []
 //     let arr1 = []
 //     console.log(arr==arr)
@@ -270,25 +272,25 @@
 
 // console.log(typeof(typeof(true)))
 
-let arr = [2,"hello",true]
+// let arr = [2,"hello",true]
 
- let data1 = arr.filter(function(a,b,c){
-    return(typeof(a===Number))
-});
+//  let data1 = arr.filter(function(a,b,c){
+//     return(typeof(a===Number))
+// });
 
-console.log(data1)
+// console.log(data1)
 
-    let data2 = arr.filter(function(a,b,c){
-        return(typeof(a===String))
-    })
+//     let data2 = arr.filter(function(a,b,c){
+//         return(typeof(a===String))
+//     })
 
-console.log(data2)
+// console.log(data2)
 
-    let data3 = arr.filter(function(a,b,c){
-        return(typeof(a===Boolean))
-    })
+//     let data3 = arr.filter(function(a,b,c){
+//         return(typeof(a===Boolean))
+//     })
 
-console.log(data2)
+// console.log(data2)
 
 
 ////////////////////////////////////////////
@@ -323,27 +325,90 @@ console.log(data2)
 // console.log(a); 
 
 
-let arr2 = [23,45,56,78,896,5,2,"hello","hi","hello","raj","can","do",true,false,true,false,true]; 
+// let arr = [23,45,56,78,896,5,2,"hello","hi","hello","raj","can","do",true,false,true,false,true];
 
-let data8 = arr2.filter(function(a,b,c){
-    return typeof a === "boolean";
-})
+// function getString(i){
+//     return typeof i === "string";
+// }
 
-console.log(data8);
+// function getNum(i){
+//     return typeof i === "number";
+// }
 
-let data9 = arr2.filter(function(a,b,c){
-    return typeof a === "String";
-})
+// function getBool(i){
+//     return typeof i === "boolean";
+// }
 
-console.log(data9);
+// function getFun(arr, f1, f2, f3){
+//     let res = [];
+//     for(let i of arr)
+//     {
+//         if(f1(i)){
+//             res.push(i);
+//         }
+//         if(f2(i)){
+//             res.push(i);
+//         }
+//         if(f3(i)){
+//             res.push(i);
+//         }
+//     }
+//     return res;
+// }
 
-let data10 = arr2.filter(function(a,b,c){
-    return typeof a === "Number";
-})
+// console.log(getFun(arr, getString, getNum, getBool));
 
-console.log(data10);
+// //////////////////////////////
 
-num
+
+// let arr1 = [23,45,56,78,896,5,2,"hello","hi","hello","raj","can","do",true,false,true,false,true];
+
+// function getFun(arr1, ...checks){
+//     let res = [];
+//     for(let i of arr1){
+//         for (let check of checks) {
+//             if(check(i)){
+//                 res.push(i);
+//                 break; // ek type match ho गया तो आगे check की जरूरत नहीं
+//             }
+//         }
+//     }
+//     return res;
+// }
+
+// // Define checks
+// const isString = i => typeof i === "string";
+// const isNumber = i => typeof i === "number";
+// const isBoolean = i => typeof i === "boolean";
+
+// // Call
+// console.log(getFun(arr1, isString, isNumber, isBoolean));
+
+
+// const result = arr1.filter(i => ["string","number","boolean"].includes(typeof i));
+// console.log(result);
+
+
+
+
+// let data8 = arr2.filter(function(a,b,c){
+//     return typeof a === "boolean";
+// })
+
+// console.log(data8);
+
+// let data9 = arr2.filter(function(a,b,c){
+//     return typeof a === "String";
+// })
+
+// console.log(data9);
+
+// let data10 = arr2.filter(function(a,b,c){
+//     return typeof a === "Number";
+// })
+
+// console.log(data10);
+
 
 
 
@@ -354,6 +419,32 @@ num
 // }
 
 
+/// this is a code of higher order function and closers and 
+
+/// higher order function are that type of functioni which returns function and get function as a argument 
+
+//// closers is a function that rembers and can access its lexical scope even when that function is executed 
+
+// function outer(){
+//     let user = "rajnesh"
+//     function inner(){
+//         console.log(user)
+//     }
+//     return inner
+// }
+// let b = outer()
+// b()
+for(var i=1;i<6;i++)
+{
+    setTimeout(() => {
+        console.log(i)
+    }, 1000*i);
+}
 
 
-
+for(let i=1;i<6;i++)
+{
+    setTimeout(() => {
+        console.log(i)
+    }, 10000*i);
+}
